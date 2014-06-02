e9918c5b33228b90de56b93b094f09e063b5ea0f
mossowski <lcinek18@gmail.com>
2014-06-02 18:18:25 +0200

    add canvas to stage

--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -1,3 +1,21 @@
-$(function () {
-    
+$(document).ready(function() {
+    var canvas,server,context;
+    var stageHeight = 60;
+    var stageWidth = 60;
+
+    server = window.location.host.name;
+    canvas = $("#stage");
+    context = canvas.get(0).getContext("2d"); 
+
+    var draw = function() {
+        var x,y;
+        context.fillStyle = '#A9D0F5 ';   
+        for (x = 0; x <= stageWidth; x++) {
+            for (y = 0; y <= stageHeight; y++) {
+                context.fillRect(x * 10, y * 10, 9, 9);
+            }
+        }
+    };
+
+    draw();   
 });

0ce0cf7a1c8f2e9c32a743457202c1753d733ca6
mossowski <lcinek18@gmail.com>
2014-06-15 17:12:25 +0200

    optimization

--- public/js/skrypt.js
+++ public/js/skrypt.js
@@ -15,12 +15,13 @@ $(document).ready(function() {
 
         canvas.width = window.innerWidth;
         canvas.height = window.innerHeight;
+
         var  snake, fud, x, y, i, j;
         var bestScore = 0;
         var xMiddle = canvas.width/2;  //srodek ekranu szerokosc
         var yMiddle = canvas.height/2;  //srodek ekranu wysokosc
-        var xSnake, ySnake;
-        var xPrzesun, yPrzesun;
+        var xSnake, ySnake;    //pozycja weza
+        var xPrzesun, yPrzesun;  //przesuniecie
 
         // pozycja weza
         for (i = 0; i < snakes.length; i++) {
@@ -29,15 +30,15 @@ $(document).ready(function() {
             if (snake.id === id) {
                 xSnake = snake.elements[snake.length-1][0] * 10;
                 ySnake = snake.elements[snake.length-1][1] * 10;
-                //console.log("snake x: " + xSnake + " y: " + ySnake);
+                //console.log("My snake x: " + xSnake + " y: " + ySnake);
             }
         } 
 
         // obliczanie przesuniecia 
         xPrzesun = xMiddle - xSnake;
         yPrzesun = yMiddle - ySnake;
-
         //console.log("srodek ekranu x: " + canvas.width/2 + " y: " + canvas.height/2);
+        
         //mapa
         context.fillStyle = '#FFFFFF';     
         context.fillRect(0, 0, canvas.width, canvas.height);
@@ -45,8 +46,14 @@ $(document).ready(function() {
         //food   
         for (i = 0; i < food.length; i++) {
             fud = food[i];
-            context.fillStyle = '#27B841'; 
-            context.fillRect(fud.x * 10 + xPrzesun, fud.y * 10 + yPrzesun, 9, 9);      
+                
+            if (fud.x * 10 < xSnake + canvas.width/2 && fud.x * 10 > xSnake - canvas.width/2) {
+                if(fud.y * 10 < ySnake + canvas.height/2 && fud.y * 10 > ySnake - canvas.height/2) {
+                    context.fillStyle = '#27B841'; 
+                    context.fillRect(fud.x * 10 + xPrzesun, fud.y * 10 + yPrzesun, 9, 9); 
+                    //console.log("draw food x: " + fud.x * 10 + " y: " + fud.y * 10);
+                } 
+            }   
         } 
  
         //snake
@@ -67,7 +74,13 @@ $(document).ready(function() {
             for (j = 0; j < snake.length; j++) {
                 x = snake.elements[j][0] * 10;
                 y = snake.elements[j][1] * 10;
-                context.fillRect(x + xPrzesun, y + yPrzesun, 9,9);
+
+                if (x < xSnake + canvas.width/2 && x > xSnake - canvas.width/2) {
+                    if(y < ySnake + canvas.height/2 && y > ySnake - canvas.height/2) {
+                        context.fillRect(x + xPrzesun, y + yPrzesun, 9,9);
+                        //console.log("draw snake x: " + x + " y: " + y);
+                    }
+                }
             }
         }       
     };
